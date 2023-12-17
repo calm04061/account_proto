7ec2998a4817d95e140f68e9157edc15d51cb3f5
Calm0406 <calm0406@163.com>
2023-12-17 12:20:32 +0800

    tonic

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,21 @@
-use std::fs::File;
-use std::io::Read;
-use std::sync::Arc;
-use grpc::{ClientStub, ClientStubExt};
-use tls_api::{Certificate, TlsAcceptorBuilder, TlsConnector, TlsConnectorBuilder};
-use crate::account_grpc::AccountClient;
-
-pub mod account;
-pub mod account_grpc;
-
-
-pub fn tls_connector(ssl_path: &str) -> tls_api_native_tls::TlsConnector {
-    let mut f = File::open(ssl_path).unwrap();
-    let mut root_ca: Vec<u8> = Vec::new();
-    let result = f.read_to_end(&mut root_ca);
-    match result {
-        Ok(_) => {
-            // let root_ca = include_bytes!("root-ca.der");
-            let certificate = Certificate::from_der(root_ca);
-            let mut builder = tls_api_native_tls::TlsConnector::builder().unwrap();
-            builder
-                .add_root_certificate(certificate)
-                .expect("add_root_certificate");
-            builder.build().unwrap()
-        }
-        Err(_) => {
-            panic!("")
-        }
-    }
+use prost::Name;
+tonic::include_proto!("account");
+impl Name for AccountReply {
+    const NAME: &'static str = "AccountReply";
+    const PACKAGE: &'static str = "account_proto";
 }
 
-pub fn tls_acceptor(ssl_path: &str, password: &str) -> tls_api_native_tls::TlsAcceptor {
-    let mut f = File::open(ssl_path).unwrap();
-    let mut pkcs12: Vec<u8> = Vec::new();
-    let result = f.read_to_end(&mut pkcs12);
-    match result {
-        Ok(_) => {
-            let builder = tls_api_native_tls::TlsAcceptorBuilder::from_pkcs12(pkcs12.as_slice(), password).unwrap();
-            builder.build().unwrap()
-        }
-        Err(_) => {
-            panic!("")
-        }
-    }
+impl Name for ClientInfoReplay {
+    const NAME: &'static str = "ClientInfoReplay";
+    const PACKAGE: &'static str = "account_proto";
 }
 
-pub fn new_account_client(tls: bool, account_grpc_address: &str, grpc_port: u16, ssl_path: &str) -> AccountClient {
-    return if tls {
-        // This is a bit complicated, because we need to explicitly pass root CA here
-        // because server uses self-signed certificate.
-        let tls_option = httpbis::ClientTlsOption::Tls(
-            "foobar.com".to_owned(),
-            Arc::new(tls_connector(ssl_path)),
-        );
-        let grpc_client = Arc::new(
-            grpc::ClientBuilder::new(account_grpc_address, grpc_port)
-                .explicit_tls(tls_option)
-                .build()
-                .unwrap(),
-        );
-        AccountClient::with_client(grpc_client)
-    } else {
-        let client_conf = Default::default();
-        AccountClient::new_plain(account_grpc_address, grpc_port, client_conf).unwrap()
-    };
+impl Name for SimpleString {
+    const NAME: &'static str = "SimpleString";
+    const PACKAGE: &'static str = "account_proto";
+}
+
+impl Name for AuthTokenReply {
+    const NAME: &'static str = "AuthTokenReply";
+    const PACKAGE: &'static str = "account_proto";
 }
