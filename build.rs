0032426b42bdfd18e9b14f9f76062d9dd6e75619
Calm0406 <calm0406@163.com>
2025-08-09 11:04:39 +0800

    upgrade

--- build.rs
+++ build.rs
@@ -1,4 +1,8 @@
+use tonic_prost_build::configure;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/account.proto")?;
+    configure()
+        .compile_protos(&["proto/account.proto"], &["proto"])
+        .unwrap();
     Ok(())
 }
