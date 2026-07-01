fe907ea956be9e25fe57a25525fb51a2d4fa51a6
Calm0406 <calm0406@163.com>
2026-07-01 22:41:13 +0800

    Jwks

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,11 @@ impl Name for ClientInfoReplay {
     const PACKAGE: &'static str = "account_proto";
 }
 
+impl Name for CountReply {
+    const NAME: &'static str = "CountReply";
+    const PACKAGE: &'static str = "account_proto";
+}
+
 impl Name for SimpleString {
     const NAME: &'static str = "SimpleString";
     const PACKAGE: &'static str = "account_proto";
@@ -17,5 +22,25 @@ impl Name for SimpleString {
 
 impl Name for AuthTokenReply {
     const NAME: &'static str = "AuthTokenReply";
+    const PACKAGE: &'static str = "account_proto";
+}
+
+impl Name for ClientInfoPageReply {
+    const NAME: &'static str = "ClientInfoPageReply";
+    const PACKAGE: &'static str = "account_proto";
+}
+
+impl Name for ClientInfoAddRequest {
+    const NAME: &'static str = "ClientInfoAddRequest";
+    const PACKAGE: &'static str = "account_proto";
+}
+
+impl Name for ClientInfoUpdateRequest {
+    const NAME: &'static str = "ClientInfoUpdateRequest";
+    const PACKAGE: &'static str = "account_proto";
+}
+
+impl Name for ClientInfoPageRequest {
+    const NAME: &'static str = "ClientInfoPageRequest";
     const PACKAGE: &'static str = "account_proto";
 }
